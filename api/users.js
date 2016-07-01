96caa80c0f8c70ffb0457d45e506789c79fe92fb
austinsprague <sprague.ak@gmail.com>
2016-06-30 21:53:13 -0700

    users updating and deleting

--- api/users.js
+++ api/users.js
@@ -29,5 +29,47 @@ router.post('/', function(req, res){
   })
 });
 
+router.get('/:id', function (req, res) {
+  queries.Users().where({ id: req.params.id }).first().then(function(data){
+    res.json(data);
+  }).catch(function(err){
+    res.json(err);
+  });
+});
+
+router.get('/:id/update', function (req, res) {
+  queries.Users().where({ id: req.params.id }).first().then(function(data){
+    res.json(data);
+  }).catch(function(err){
+    res.json(err);
+  });
+});
+
+router.put('/:id/update', function (req, res) {
+  queries.Users().where({ id: req.params.id }).update({
+    name: 'Betty Boop',
+    address: '111 Pine St, SF CA',
+    phone_num: 2069991123,
+    background: 'Ultimate baking champion',
+    username: 'betty_shop',
+    password: '123456',
+    credit_card: 1111222333444,
+    cvv: 123,
+    exp: 0219,
+    zip: 99999
+  }).then(function(data){
+    res.json('successful update');
+  }).catch(function(err){
+    res.json(err);
+  });
+});
+
+router.delete('/:id/delete', function(req, res) {
+  queries.Users().where({ id: req.params.id }).del().then(function(){
+    res.json('successful delete');
+  }).catch(function(err){
+    res.json(err);
+  });
+});
 
 module.exports = router;
